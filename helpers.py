278c24dd991331628c15620b64c7c880588adc7a
henry ehlers <henry.ward.ehlers@gmail.com>
2021-04-07 09:21:23 +0200

    added maps  and test

--- helpers.py
+++ helpers.py
@@ -1,91 +1,24 @@
-import networkx as nx
-import pickle
-import plotly.plotly as py
-import random
-from plotly.graph_objs import *
-from plotly.offline import init_notebook_mode, plot, iplot
-init_notebook_mode(connected=True)
+from helpers import load_map
 
-class Map:
-	def __init__(self, G):
-		self._graph = G
-		self.intersections = nx.get_node_attributes(G, "pos")
-		self.roads = [list(G[node]) for node in G.nodes()]
+MAP_40_ANSWERS = [
+    (5, 34, [5, 16, 37, 12, 34]),
+    (5, 5,  [5]),
+    (8, 24, [8, 14, 16, 37, 12, 17, 10, 24])
+]
 
-	def save(self, filename):
-		with open(filename, 'wb') as f:
-			pickle.dump(self._graph, f)
-
-def load_map(name):
-	with open(name, 'rb') as f:
-		G = pickle.load(f)
-	return Map(G)
-
-def show_map(M, start=None, goal=None, path=None):
-    G = M._graph
-    pos = nx.get_node_attributes(G, 'pos')
-    edge_trace = Scatter(
-    x=[],
-    y=[],
-    line=Line(width=0.5,color='#888'),
-    hoverinfo='none',
-    mode='lines')
-
-    for edge in G.edges():
-        x0, y0 = G.node[edge[0]]['pos']
-        x1, y1 = G.node[edge[1]]['pos']
-        edge_trace['x'] += [x0, x1, None]
-        edge_trace['y'] += [y0, y1, None]
-
-    node_trace = Scatter(
-        x=[],
-        y=[],
-        text=[],
-        mode='markers',
-        hoverinfo='text',
-        marker=Marker(
-            showscale=False,
-            # colorscale options
-            # 'Greys' | 'Greens' | 'Bluered' | 'Hot' | 'Picnic' | 'Portland' |
-            # Jet' | 'RdBu' | 'Blackbody' | 'Earth' | 'Electric' | 'YIOrRd' | 'YIGnBu'
-            colorscale='Hot',
-            reversescale=True,
-            color=[],
-            size=10,
-            colorbar=dict(
-                thickness=15,
-                title='Node Connections',
-                xanchor='left',
-                titleside='right'
-            ),
-            line=dict(width=2)))
-    for node in G.nodes():
-        x, y = G.node[node]['pos']
-        node_trace['x'].append(x)
-        node_trace['y'].append(y)
-
-    for node, adjacencies in enumerate(G.adjacency_list()):
-        color = 0
-        if path and node in path:
-            color = 2
-        if node == start:
-            color = 3
-        elif node == goal:
-            color = 1
-        # node_trace['marker']['color'].append(len(adjacencies))
-        node_trace['marker']['color'].append(color)
-        node_info = "Intersection " + str(node)
-        node_trace['text'].append(node_info)
-
-    fig = Figure(data=Data([edge_trace, node_trace]),
-                 layout=Layout(
-                    title='<br>Network graph made with Python',
-                    titlefont=dict(size=16),
-                    showlegend=False,
-                    hovermode='closest',
-                    margin=dict(b=20,l=5,r=5,t=40),
-                   
-                    xaxis=XAxis(showgrid=False, zeroline=False, showticklabels=False),
-                    yaxis=YAxis(showgrid=False, zeroline=False, showticklabels=False)))
-
-    iplot(fig)
+def test(shortest_path_function):
+    map_40 = load_map('map-40.pickle')
+    correct = 0
+    for start, goal, answer_path in MAP_40_ANSWERS:
+        path = shortest_path_function(map_40, start, goal)
+        if path == answer_path:
+            correct += 1
+        else:
+            print("For start:", start, 
+                  "Goal:     ", goal,
+                  "Your path:", path,
+                  "Correct:  ", answer_path)
+    if correct == len(MAP_40_ANSWERS):
+        print("All tests pass! Congratulations!")
+    else:
+        print("You passed", correct, "/", len(MAP_40_ANSWERS), "test cases")
